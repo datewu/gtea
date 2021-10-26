bf40f355d16e9bb20a015b8781c2517c220c7bf4
datewu <hihahajun@gmail.com>
2021-10-26 14:23:57 +0800

    add pagination type

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/datewu/gtea
 
 go 1.17
 
-require (
-	github.com/datewu/jsonlog v0.0.2
-	github.com/datewu/validator v0.0.2
-)
+require github.com/datewu/jsonlog v0.0.2
---
+++ pagination.go
@@ -0,0 +1,37 @@
+package gtea
+
+// Pagination represents a pagination object.
+type Pagination struct {
+	page    int
+	perPage int
+}
+
+// NewPagination returns a new Pagination object.
+func NewPagination(page, perPage int) *Pagination {
+	if page < 1 {
+		page = 1
+	}
+	if perPage < 1 {
+		perPage = 20
+	}
+	return &Pagination{
+		page:    page,
+		perPage: perPage,
+	}
+}
+
+func (p *Pagination) SetPage(page int) {
+	if page < 1 {
+		p.page = 1
+		return
+	}
+	p.page = page
+}
+
+func (p *Pagination) Limit() int {
+	return p.perPage
+}
+
+func (p *Pagination) Offset() int {
+	return (p.page - 1) * p.perPage
+}
