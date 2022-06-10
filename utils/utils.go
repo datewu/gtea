50719ba8680d86aa3b32965b99e83dc55058fed7
datewu <hihahajun@gmail.com>
2022-06-10 11:05:19 +0800

    add helper func

--- utils/utils.go
+++ utils/utils.go
@@ -7,3 +7,11 @@ func InGithubCI() bool {
 	// https://help.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables
 	return os.Getenv("GITHUB_ACTION") != ""
 }
+
+// PanicIfErr call fn panic if err is not nil
+func PanicIfErr(fn func() error) {
+	err := fn()
+	if err != nil {
+		panic(err)
+	}
+}
