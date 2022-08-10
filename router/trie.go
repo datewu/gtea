ea0ccdefaf9a98309cdb6ec84c1aa48870a1fe34
datewu <hihahajun@gmail.com>
2022-08-10 18:55:06 +0800

    stag

--- router/trie.go
+++ router/trie.go
@@ -1,8 +1,19 @@
 package router
 
 import (
+	"context"
 	"net/http"
 	"strings"
+)
+
+type pathRegs string
+type pathName string
+
+const paramsCtxKey pathRegs = "path_param_names"
+const (
+	pathSeperator   = "/"
+	paramNote       = ":"
+	specialChildKey = "  "
 )
 
 func newPathTrie() *pathTrie {
@@ -19,40 +30,75 @@ type pathTrie struct {
 }
 
 func (p *pathTrie) get(path string) http.Handler {
-	parts := strings.Split(path, "/")
+	parts := strings.Split(strings.TrimSpace(path), pathSeperator)
 	node := p
+	paramValues := []string{}
 	for _, v := range parts {
 		child, ok := node.children[v]
 		if !ok {
+			if s, ok := node.children[specialChildKey]; ok {
+				paramValues = append(paramValues, v)
+				node = s
+				continue
+			}
 			return nil
-		} else {
-			node = child
 		}
+		node = child
+	}
+	if len(paramValues) > 0 {
+		if node.value == nil {
+			return nil
+		}
+		wrapH := func(w http.ResponseWriter, r *http.Request) {
+			keys := r.Context().Value(paramsCtxKey)
+			ks, ok := keys.([]string)
+			if !ok || len(paramValues) != len(ks) {
+				return
+			}
+			ctx := r.Context()
+			for i := range ks {
+				ctx = context.WithValue(ctx, pathName(ks[i]), paramValues[i])
+			}
+			r = r.WithContext(ctx)
+			node.value.ServeHTTP(w, r)
+		}
+		return http.HandlerFunc(wrapH)
 	}
 	return node.value
 }
 
 // suffix '/' counts: path '/a' is diffent from path '/a/'
 func (p *pathTrie) put(path string, value http.Handler) {
-	parts := strings.Split(path, "/")
+	parts := strings.Split(strings.TrimSpace(path), pathSeperator)
 	node := p
+	regs := []string{}
 	for _, v := range parts {
-		// not skip '/' suffix
-		// if v == "" {
-		// 	continue
-		// }
 		child, ok := node.children[v]
 		if !ok {
 			child = &pathTrie{
 				value:    nil,
 				children: make(map[string]*pathTrie),
 			}
-			node.children[v] = child
+			if strings.HasPrefix(v, paramNote) {
+				regs = append(regs, strings.TrimPrefix(v, ":"))
+				node.children[specialChildKey] = child
+			} else {
+				node.children[v] = child
+			}
 		}
 		node = child
 	}
 	if node.value != nil {
 		panic("trie node value conflict")
+	}
+	if len(regs) > 0 {
+		wrapH := func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(r.Context(), paramsCtxKey, regs)
+			r = r.WithContext(ctx)
+			value.ServeHTTP(w, r)
+		}
+		node.value = http.HandlerFunc(wrapH)
+		return
 	}
 	node.value = value
 }
